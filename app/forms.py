5fd632c01e0142b73a1cff11a417de906b7f0776
Dzemoro <dellorpl@gmail.com>
2020-06-06 20:18:43 +0200

    last

--- app/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
-
-class LoginForm(FlaskForm):
-    username = StringField('Login', validators=[DataRequired()])
-    password = PasswordField('Haslo', validators=[DataRequired()])
-    remember_me = BooleanField('Zapamietaj mnie')
-    submit = SubmitField('Zaloguj sie')
